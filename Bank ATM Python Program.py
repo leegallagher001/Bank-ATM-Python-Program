bf93675af604b24cc8dfeb2386bab8ebcc8c4416
leegallagher001 <lee.gallagher.writing@outlook.com>
2025-10-13 18:08:04 +0100

    Started work on a basic Python ATM program with withdrawal and deposit functionality

---
+++ Bank ATM Python Program.py
@@ -0,0 +1,93 @@
+# Bank ATM Program - by Lee Gallagher
+
+# This program is designed to replicate the experience of using an ATM that has the ability to allow the user to both withdraw and deposit cash
+
+# (1) Opening Statements
+
+print("Welcome to Redstone Bank!")
+print("\n")
+pin_number = int(input("Please Enter Your PIN: "))  # to keep things simple, the initial PIN is 1234
+print("\n")
+
+# (2) Validate PIN
+
+if pin_number == 1234:
+    print("Hello, Dear Customer. Please choose which service you would like today from the following options below: ")
+    print("\n")
+    print("1. Check Balance")
+    print("2. Cash Withdrawal")
+    print("3. Cash Deposit")
+    print("4. Change PIN")
+    print("5. Exit ATM")
+else:
+    print("Invalid PIN.")
+    input("Press 'ENTER' to exit the program.")
+
+# (3) Define Initial Variable Values & Functions
+
+balance = 500
+
+def checkBalance():
+    global balance
+    print("\n")
+    print("Current Balance: \xa3", balance)
+
+def cashWithdrawal():
+    global balance
+    print("\n")
+    print("Both \xa310 and \xa320 notes are available.")
+    print("\n")
+    withdraw_amount = int(input("How much would you like to withdraw?: "))
+    if withdraw_amount <= balance:
+        balance -= withdraw_amount
+        print("\n")
+        print("Successfully withdrawn \xa3", withdraw_amount, ". You have \xa3", balance, " remaining.")
+    else:
+        print("\n")
+        print("Insufficient funds for transaction.")
+
+def cashDeposit():
+    global balance
+    print("\n")
+    deposit_amount = int(input("How much would you like to deposit?: "))
+    balance += deposit_amount
+    print("Successfully deposited \xa3", deposit_amount, ". You have \xa3", balance, " remaining.")
+
+def changePIN():
+    global pin_number
+    print("\n")
+    verify_current_pin = int(input("Enter Your Current PIN: "))
+    if verify_current_pin == pin_number:
+        pin_number = int(input("Enter A New PIN Number: "))
+
+# (4) Activate Menu (Call Functionality)
+
+menuChoice = int(input("Which Option Do You Choose?: "))
+
+while menuChoice != 5:
+    if menuChoice == 1:
+        checkBalance()
+        print("\n")
+        menuChoice = int(input("Looking for something else? Choose another menu option: "))
+    elif menuChoice == 2:
+        cashWithdrawal()
+        print("\n")
+        menuChoice = int(input("Looking for something else? Choose another menu option: "))
+    elif menuChoice == 3:
+        cashDeposit()
+        print("\n")
+        menuChoice = int(input("Looking for something else? Choose another menu option: "))
+    elif menuChoice == 4:
+        changePIN()
+        print("\n")
+        menuChoice = int(input("Looking for something else? Choose another menu option: "))
+    else:
+        print("Invalid Input")
+        print("\n")
+        menuChoice = int(input("Looking for something else? Choose another menu option: "))
+
+print("\n")
+print("Thanks for using the Redstone Bank ATM!")
+print("\n")
+print("Press 'ENTER' to exit the program.")
+
